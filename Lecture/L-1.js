072bd9df98b0f5c88f106147c1ca8b910ad9e05f
Rishi Raj Gautam <ritu851130@gmail.com>
2023-05-24 19:47:01 +0530

    first commit

---
+++ Lecture/L-1.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+let name= "Ritu";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <img src="" alt="" />
+      <div className="container">
+        <h1>My name is {name}</h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+        voluptatum voluptate doloremque cum illo modi optio quod iste! Quam
+        veritatis architecto cum natus recusandae, dolorum, eaque, hic quae in
+        beatae sapiente omnis?
+      </div>
+    </>
+  );
+}
+
+export default App;
